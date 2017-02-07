8b48d4d83b39f3e5d0582400252e57b61be98bbe
wayne <WayneZhouY@gmail.com>
2017-02-08 03:44:48 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,41 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Wayne Zhou on 2017/2/8.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+    public int getCityCode() {
+        return cityCode;
+    }
+    public int getProvinceId() {
+        return provinceId;
+    }
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
