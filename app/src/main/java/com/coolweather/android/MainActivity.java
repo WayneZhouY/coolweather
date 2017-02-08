53c83c2186627b0c4025d165224722abe415381a
wayne <WayneZhouY@gmail.com>
2017-02-08 08:29:35 +0800

    完成了天气预报功能，其中有网络、JSON解析、数据库

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,7 +1,12 @@
 package com.coolweather.android;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.coolweather.android.gson.Weather;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        //如果不是第一次打开，直接跳转到weather界面
+        if(prefs.getString("weather",null)!=null){
+            Intent intent=new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
